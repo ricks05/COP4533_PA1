3a44c3807f24b0cbec9b044d9f227429e4d21818
Rick Silva <hbsilva2005@gmail.com>
2026-01-28 21:05:01 -0500

    initial part A commit

--- src/Matching.cpp
+++ src/Matching.cpp
@@ -1,5 +1,62 @@
-//
-// Created by hbsil on 1/28/2026.
-//
-
 #include "Matching.h"
+
+vector<int> Matching::matchingEngine() {
+    for (int h = 1; h <= n; h++) {
+        cout << h << endl;
+        if (numMatches == n)
+            break;
+        if (hMatches[h] != 0 || hTries[h] > n)
+            continue;
+        int s = hPref[h][hTries[h]++];
+        if (sMatches[s] == 0) {
+            hMatches[h] = s;
+            sMatches[s] = h;
+            numMatches++;
+        }
+        int hPrime = sMatches[s];
+        for (int i = 0; i < n ; i++) {
+            if (sPref[s][i] == hPrime) {
+                break;
+            }
+            else if (sPref[s][i] == h) {
+                hMatches[h] = s;
+                sMatches[s] = h;
+                hMatches[hPrime] = 0;
+                break;
+            }
+        }
+        if (h == n) {
+            h = 0;
+        }
+    }
+
+    return hMatches;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    auto h = vector(n+1, vector<int>(n));
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> h[i][j];
+        }
+    }
+
+    auto s = vector(n+1, vector<int>(n));
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> s[i][j];
+        }
+    }
+
+    Matching matchObj(n, h, s);
+    vector<int> res = matchObj.matchingEngine();
+
+    for (int i = 1; i <= n; i++) {
+        cout << i << " " << res[i] << endl;
+    }
+
+    return 0;
+}
--- src/Matching.h
+++ src/Matching.h
@@ -1,14 +1,16 @@
-//
-// Created by hbsil on 1/28/2026.
-//
-
-#ifndef COP4533_PA1_MATCHING_H
-#define COP4533_PA1_MATCHING_H
-
+#include <vector>
+#include <iostream>
+#pragma once
+using namespace std;
 
 class Matching {
-
+    int n;
+    vector<vector<int>> hPref, sPref; // each row represents a hospital/student, each column is that entity's preferred student/hospital
+    vector<int> hMatches, sMatches, hTries; // each index is a hospital/student, where its elements are their matched student/hospital
+    int numMatches;
+public:
+    Matching(int n, vector<vector<int>>& h, vector<vector<int>>& s)
+        : n(n), hPref(h), sPref(s), hMatches(n+1, 0), sMatches(n+1, 0),
+          hTries(n+1, 0), numMatches(0) {}
+    vector<int> matchingEngine();
 };
-
-
-#endif //COP4533_PA1_MATCHING_H
